cfb60a71e8c2bffd3f6a355c4341c34308887bdf
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2022-07-15 19:38:48 +0200

    center-box-name

---
+++ src/component/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeStyle.css';
+
+function Home (){
+    return (<>
+    <body>
+        <div className='background'>
+        <div className='center-box-name'>
+            <p>Charles Cantin</p>
+        </div>
+        </div>
+    </body>
+    </>);
+}
+
+export default Home;
