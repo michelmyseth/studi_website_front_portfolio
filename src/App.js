3ede243d48df8ca9818e1b04b4fa205ece94deeb
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2022-07-19 21:18:38 +0200

    add nav bar + footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Footer from './component/footer/Footer';
 import Home from './component/home/Home';
+import Navbar from './component/navbar/Navbar';
+import {Routes, Route} from "react-router-dom";
+import Galerie from './component/galerie/Galerie';
+
 
 function App() {
   return (
-   <Home/>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Galerie" element={<Galerie/>}/>
+    </Routes>
+    <Footer/>
+    </>
    );
 }
 
