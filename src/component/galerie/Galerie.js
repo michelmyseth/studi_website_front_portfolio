634c1f3f197733bbc7390ae54bd0816e94224f9d
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2022-07-21 19:39:24 +0200

    add sheet gallery

--- src/component/galerie/Galerie.js
+++ src/component/galerie/Galerie.js
@@ -1,24 +1,150 @@
-import React from "react";
-import './GalerieStyle.css';
+import React, { useEffect, useState } from "react";
+import "./GalerieStyle.css";
+import img2 from "../../assets/img2.jpg";
+import img3 from "../../assets/img3.jpg";
+import img4 from "../../assets/img4.jpg";
+import img5 from "../../assets/img5.jpg";
+import img6 from "../../assets/img6.jpg";
+import img7 from "../../assets/img7.jpg";
+import img8 from "../../assets/img8.jpg";
+import img9 from "../../assets/img9.jpg";
+import img10 from "../../assets/img10.jpg";
+import img11 from "../../assets/img11.jpg";
+import img12 from "../../assets/img12.jpg";
+import img13 from "../../assets/img13.jpg";
+import img14 from "../../assets/img14.jpg";
+import img15 from "../../assets/img15.jpg";
+import img16 from "../../assets/img16.jpg";
+import img17 from "../../assets/img17.jpg";
+import img18 from "../../assets/img18.jpg";
+import img19 from "../../assets/img19.jpg";
+import img20 from "../../assets/img20.jpg";
+import img21 from "../../assets/img21.jpg";
+import img22 from "../../assets/img22.jpg";
+import img23 from "../../assets/img23.jpg";
+import img24 from "../../assets/img24.jpg";
+import img25 from "../../assets/img25.jpg";
+import img26 from "../../assets/img26.jpg";
+import img27 from "../../assets/img27.jpg";
+import img28 from "../../assets/img28.jpg";
+import img29 from "../../assets/img29.jpg";
+import img30 from "../../assets/img30.jpg";
+import img31 from "../../assets/img31.jpg";
+import img32 from "../../assets/img32.jpg";
+import img33 from "../../assets/img33.jpg";
 
-function Galerie(){
-    return(
-        <div className="content-container">
-        <h5>wewqeqw</h5>
-        <p>What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+import { SRLWrapper } from "simple-react-lightbox";
 
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+const images = [
+    { imageName: img2, tag: "mariage" },
+    { imageName: img3, tag: "mariage" },
+    { imageName: img4, tag: "mariage" },
+    { imageName: img5, tag: "mariage" },
+    { imageName: img6, tag: "mariage" },
+    { imageName: img7, tag: "mariage" },
+    { imageName: img8, tag: "mariage" },
+    { imageName: img9, tag: "mariage" },
+    { imageName: img10, tag: "bebe" },
+    { imageName: img11, tag: "bebe" },
+    { imageName: img12, tag: "bebe" },
+    { imageName: img13, tag: "bebe" },
+    { imageName: img14, tag: "bebe" },
+    { imageName: img15, tag: "bebe" },
+    { imageName: img16, tag: "bebe" },
+    { imageName: img17, tag: "bebe" },
+    { imageName: img18, tag: "famille" },
+    { imageName: img19, tag: "famille" },
+    { imageName: img20, tag: "famille" },
+    { imageName: img21, tag: "famille" },
+    { imageName: img22, tag: "bapteme" },
+    { imageName: img23, tag: "bapteme" },
+    { imageName: img24, tag: "bapteme" },
+    { imageName: img25, tag: "bapteme" },
+    { imageName: img26, tag: "couple" },
+    { imageName: img27, tag: "couple" },
+    { imageName: img28, tag: "couple" },
+    { imageName: img29, tag: "couple" },
+    { imageName: img30, tag: "grossesse" },
+    { imageName: img31, tag: "grossesse" },
+    { imageName: img32, tag: "grossesse" },
+    { imageName: img33, tag: "grossesse" },
+];
 
-
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. </p>
-
-    </div>
-    )
+function Galerie() {
+    const [tag, setTag] = useState("all");
+    const [filteredImages, setFilteredImages] = useState([]);
+    useEffect(() => {
+        tag === "all"
+            ? setFilteredImages(images)
+            : setFilteredImages(images.filter((image) => image.tag === tag));
+    }, [tag]);
+    return (
+        <div className="App" handleSetTag={setTag}>
+            <div className="tags">
+                <TagButton
+                    name="all"
+                    handleSetTag={setTag}
+                    tagActive={tag === "all" ? true : false}
+                />
+                <TagButton
+                    name="mariage"
+                    handleSetTag={setTag}
+                    tagActive={tag === "mariage" ? true : false}
+                />
+                <TagButton
+                    name="bebe"
+                    handleSetTag={setTag}
+                    tagActive={tag === "bebe" ? true : false}
+                />
+                <TagButton
+                    name="famille"
+                    handleSetTag={setTag}
+                    tagActive={tag === "famille" ? true : false}
+                />
+                <TagButton
+                    name="bapteme"
+                    handleSetTag={setTag}
+                    tagActive={tag === "bapteme" ? true : false}
+                />
+                <TagButton
+                    name="couple"
+                    handleSetTag={setTag}
+                    tagActive={tag === "couple" ? true : false}
+                />
+                <TagButton
+                    name="grossesse"
+                    handleSetTag={setTag}
+                    tagActive={tag === "grossesse" ? true : false}
+                />
+            </div>
+            <br /> <br />
+            <SRLWrapper>
+                <div className="container-image">
+                    {filteredImages.map((image, index) => (
+                        <div key={index + 1} className="image-card">
+                            <a href={image.imageName}>
+                                <img
+                                    className="image"
+                                    src={image.imageName}
+                                    alt=""
+                                />
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            </SRLWrapper>
+        </div>
+    );
 }
 
+const TagButton = ({ name, handleSetTag, tagActive }) => {
+    return (
+        <button
+            className={`tag ${tagActive ? "active" : null}`}
+            onClick={() => handleSetTag(name)}
+        >
+            {name.toUpperCase()}
+        </button>
+    );
+};
 export default Galerie;
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,46 +1,48 @@
 import React from "react";
-import './NavbarStyle.css';
-import {Link} from 'react-router-dom'
+import "./NavbarStyle.css";
+import { Link } from "react-router-dom";
 
-function Navbar(){
+function Navbar() {
     return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-<li className="nav-link"><Link to="/">
-          Home </Link>
-        </li>
-  <button
-    className="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarNavDropdown"
-    aria-controls="navbarNavDropdown"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse justify-content-center navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <li className="nav-link"><Link to="/galerie">
-          Galerie </Link>
-        </li>
-        
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="c">
-           Tarifs et Prestations
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="d">
-          Contact
-        </a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    )
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <li className="nav-link">
+                <Link to="/">Home </Link>
+            </li>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+                className="collapse justify-content-center navbar-collapse"
+                id="navbarNavDropdown"
+            >
+                <ul className="navbar-nav">
+                    <li className="nav-item ">
+                        <li className="nav-link">
+                            <Link to="/galerie">Galerie </Link>
+                        </li>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="c">
+                            Tarifs et Prestations
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="d">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
